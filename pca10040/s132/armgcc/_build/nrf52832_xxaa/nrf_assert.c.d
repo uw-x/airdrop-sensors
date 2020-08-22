22d3d701eb1e5ece4dbe7242c9761df1b5beb9e2
Maruchi Kim <maruchi.kim@gmail.com>
2020-08-22 14:12:53 -0700

    Add makefile from master branch

---
+++ pca10040/s132/armgcc/_build/nrf52832_xxaa/nrf_assert.c.d
@@ -0,0 +1,131 @@
+_build/nrf52832_xxaa/nrf_assert.c.o: \
+ ../../../../../../components/libraries/util/nrf_assert.c \
+ ../../../../../../components/libraries/util/nrf_assert.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/lib/gcc/arm-none-eabi/7.3.1/include/stdint.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/stdint.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/machine/_default_types.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/sys/features.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/_newlib_version.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/sys/_intsup.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/sys/_stdint.h \
+ ../../../../../../modules/nrfx/mdk/nrf.h \
+ ../../../../../../modules/nrfx/mdk/nrf52.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../../components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../../modules/nrfx/mdk/system_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_bitfields.h \
+ ../../../../../../modules/nrfx/mdk/nrf51_to_nrf52.h \
+ ../../../../../../modules/nrfx/mdk/nrf52_name_change.h \
+ ../../../../../../modules/nrfx/mdk/compiler_abstraction.h \
+ ../../../../../../components/libraries/util/app_error.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/stdio.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/_ansi.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/newlib.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/sys/config.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/machine/ieeefp.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/sys/cdefs.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/lib/gcc/arm-none-eabi/7.3.1/include/stddef.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/lib/gcc/arm-none-eabi/7.3.1/include/stdarg.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/sys/reent.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/_ansi.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/sys/_types.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/machine/_types.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/sys/lock.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/sys/types.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/machine/types.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/sys/stdio.h \
+ /usr/local/Cellar/gcc-arm-none-eabi/20180627/lib/gcc/arm-none-eabi/7.3.1/include/stdbool.h \
+ ../../../../../../components/libraries/util/sdk_errors.h \
+ ../../../../../../components/softdevice/s132/headers/nrf_error.h \
+ ../../../../../../components/libraries/util/nordic_common.h \
+ ../../../../../../components/libraries/util/app_error_weak.h
+
+../../../../../../components/libraries/util/nrf_assert.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/lib/gcc/arm-none-eabi/7.3.1/include/stdint.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/stdint.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/sys/features.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/_newlib_version.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/sys/_intsup.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/sys/_stdint.h:
+
+../../../../../../modules/nrfx/mdk/nrf.h:
+
+../../../../../../modules/nrfx/mdk/nrf52.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cm4.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmInstr.h:
+
+../../../../../../components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmFunc.h:
+
+../../../../../../components/toolchain/cmsis/include/core_cmSimd.h:
+
+../../../../../../modules/nrfx/mdk/system_nrf52.h:
+
+../../../../../../modules/nrfx/mdk/nrf52_bitfields.h:
+
+../../../../../../modules/nrfx/mdk/nrf51_to_nrf52.h:
+
+../../../../../../modules/nrfx/mdk/nrf52_name_change.h:
+
+../../../../../../modules/nrfx/mdk/compiler_abstraction.h:
+
+../../../../../../components/libraries/util/app_error.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/stdio.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/_ansi.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/newlib.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/sys/config.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/machine/ieeefp.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/sys/cdefs.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/lib/gcc/arm-none-eabi/7.3.1/include/stddef.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/lib/gcc/arm-none-eabi/7.3.1/include/stdarg.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/sys/reent.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/_ansi.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/sys/_types.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/machine/_types.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/sys/lock.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/sys/types.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/sys/_pthreadtypes.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/machine/types.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/arm-none-eabi/include/sys/stdio.h:
+
+/usr/local/Cellar/gcc-arm-none-eabi/20180627/lib/gcc/arm-none-eabi/7.3.1/include/stdbool.h:
+
+../../../../../../components/libraries/util/sdk_errors.h:
+
+../../../../../../components/softdevice/s132/headers/nrf_error.h:
+
+../../../../../../components/libraries/util/nordic_common.h:
+
+../../../../../../components/libraries/util/app_error_weak.h:
